afcaafff96ea6794201ed819e1333d9cd66a73d6
Rodrigo Peixe Oliveira <114592367+rpeixe@users.noreply.github.com>
2023-12-13 07:52:42 -0300

    Adicionado loop principal

--- Main.asm
+++ Main.asm
@@ -5,26 +5,33 @@ turno: .word 0
 .text
 	.globl main
 main:
-	jal read_map_from_file
+	# $s0: Mapa
+	# $s1: Player
+	# $s2: Bit pronto
+	# $s3: Tecla apertada
+
+	jal read_map_from_file	# Le o mapa do arquivo map.txt na pasta raiz do Mars
 	move $s0, $v0
 	
 	move $a0, $s0
-	li $a1, 6
-	li $a2, 6
-	jal get_map_obj
-	move $a0, $v0
-	jal print_object
+	jal get_player
+	move $s1, $v0
 	
+	li $s2, 0xffff0000	# Ready bit
+	li $s3, 0xffff0004	# Tecla apertada
+	
+player_turn:	# Inicio do turno do player
+	move $a0, $s0
+	jal draw_map	# Atualiza o bitmap display
+wait_input:
+	lw $t0, ($s2)
+	beq $t0, 0, wait_input	# Pooling de entrada
+input_received:
+	sw $zero, ($s2)
+	move $a0, $s1
+	jal print_object
+	j player_turn
 
 	li $v0, 10	# Finaliza o programa
 	syscall
 	
-
-#turn_increment:
-	#Incrementa o turno do jogo após uma ação do jogador ou inimigo
-
-	#la $t0,turno
-	#addi $t0,$t0,1
-	#sw $t0,turno
-
-	#jr $ra
